be32fefca2caa6d36f39586b67c976c704ef596e
fedeviotti <fedeviotti@gmail.com>
2022-01-23 15:50:04 +0100

    Feat: Adding navigation with Link

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -1,5 +1,23 @@
 import * as React from 'react';
+import Link from 'next/link';
 
-const Notes = () => <div>Notes page path</div>
+const notesResult = new Array(15)
+  .fill(1)
+  .map((element, index) => ({ id: index, title: `Note: ${index}` }));
+
+const Notes = () => (
+  <div>
+    <h1>Notes page path</h1>
+    <ul>
+      {notesResult.map((note) => (
+        <li key={note.id}>
+          <Link href={`/notes/${note.id}`}>
+            <a>{note.title}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default Notes;
